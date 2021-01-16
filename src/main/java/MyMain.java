51c5573a4f04e748057b1c37b0cb108c00a5fba4
amandacutrer <23acutrer@athenian.org>
2021-01-16 20:58:54 +0000

    worked on methods

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,15 +1,45 @@
 public class MyMain {
 
     // Returns the mean of the 2D array mat
-    public static double mean(double[][] mat) { 
-        // YOUR CODE HERE
-        return -1.0;
+    public static double mean(double[][] mat) {
+        double sum = 0; 
+        int count = 0;
+        for (int row = 0; row < mat.length; row++){
+            for (int col = 0; col < mat[0].length; col++){
+                sum = sum + mat[row][col];
+                count++;
+            }
+        }
+        return sum/count;
     }
 
     // Returns the median of the 2D array mat
     public static double median(double[][] mat) { 
-        // YOUR CODE HERE
-        return -1.0;
+    double count = 0;
+    double count2 = 0;
+       for (int row = 0; row < mat.length; row++){
+            for (int col = 0; col < mat[0].length; col++){
+                count ++;
+            }
+        }
+        count = count / 2;
+            if (count % 2 == 0){count = count / 2;
+        for (int row = 0; row < mat.length; row++){
+            for (int col = 0; col < mat[0].length; col++){
+                count2++;
+                if (count == count2){return (double)(mat[row][col]+mat[row][col+1])/2.0;}
+            }
+        }
+    }
+    else {count = count/2; 
+        for (int row = 0; row < mat.length; row++){
+            for (int col = 0; col < mat[0].length; col++){
+            count2++;
+            if (count == count2){return (double)(mat[row][col+1]);}
+    }
+    }
+}
+return 1.0;
     }
     
 
@@ -21,6 +51,12 @@ public class MyMain {
 
 
     public static void main(String[] args) {
-        // Write some code here to test your methods!
+        double mat[][] = {{1, 3, 7, 10}, 
+                         {14, 18, 19, 21}, 
+                         {23, 26, 30, 31}};
+    System.out.println(mean(mat));
+    System.out.println(median(mat));
+    System.out.println(mode(mat));
+
     }
 }
